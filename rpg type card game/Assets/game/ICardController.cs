82817c7e7c7f6c723a8cb0d4ca2566255053a33c
kotatuxu <tasw50@yahoo.co.jp>
2021-06-02 01:06:32 +0900

    tes

--- rpg type card game/Assets/game/ICardController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-//
-public abstract class ICardController 
-{
-    public abstract void Init();
-    public abstract void Process();
-}
